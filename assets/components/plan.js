1cbabe051cc7c003b6398481d021c71ad8e0630d
Thomas Dubernet <thoms.dubernet@gmail.com>
2021-12-01 10:34:43 +0100

    Mise en place de la base du plan en React

--- assets/components/plan.js
+++ assets/components/plan.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { render } from 'react-dom'
+import { useGetMany } from '../hooks'
+
+const ContextMenuTable = ({table}) => {
+
+    return (
+        <div>Menu</div>
+    )
+}
+
+const Table = ({table}) => {
+    const [open, setOpen] = useState(false)
+    const {
+        id,
+        nom,
+        nombrePlacesMax: nbMax,
+        numero,
+        categorie: {
+            id: idCat,
+            couleur,
+            nom: nomCat
+        },
+        personnes
+    } = table
+    
+    return (
+        <React.Fragment>
+            <div
+                className="custom-table"
+                style={{background: couleur}}
+                onContextMenu={() => setOpen(true)}
+                onClick={() => console.log('left click')}
+            >
+                <p className="number">N°{numero}</p>
+                <p className="number-max">
+                {personnes.length} / {nbMax}
+                </p>
+            </div>
+
+            {open && <ContextMenuTable table={table} />}
+        </React.Fragment>
+    )
+}
+
+const Plan = () => {
+    const { items: tables, load, loading } = useGetMany('tables')
+
+    useEffect(() => {
+        load()
+    }, [])
+
+    return !loading && (
+        <React.Fragment>
+            <div className="d-flex">
+                {tables.length > 0
+                    ? tables.map((table, index) => (
+                        <Table key={index} table={table} />
+                    ))
+                    : <p>Aucune table n'a été crée.</p>
+                }
+            </div>
+        </React.Fragment>
+    )
+}
 
 class PlanElement extends HTMLElement {
     connectedCallback() {
-      render(<div>Test PLAN</div>, this)
+      render(<Plan />, this)
     }
   }
   
