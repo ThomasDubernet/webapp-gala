d5389ec7b0adfa9bb3075f491483f0371035a059
ThomasDubernet <thoms.dubernet@gmail.com>
2024-01-22 09:57:35 +0100

    fix: Suppression des personnes sans table

--- src/Controller/ResetController.php
+++ src/Controller/ResetController.php
@@ -142,32 +142,49 @@ class ResetController extends AbstractController
     }
 
     /**
-     * @Route("/personnes", name="reset_personnes")
+     * @Route("/personnes/reset/without-table", name="reset_personnes")
      */
     public function cleanPersonnesWithoutTable(): Response
     {
         $personnes = $this->em->getRepository(Personne::class)->findBy(['table' => null]);
+
         $tickets = [];
         $uploadDir = $this->getParameter('kernel.project_dir') . '/public' . $this->getParameter('upload_directory');
         $fs = new Filesystem();
-
-        $this->em->beginTransaction();
 
         try {
             foreach ($personnes as $personne) {
                 if ($personne->getTicket() !== null) {
                     $tickets[] = $personne->getTicket();
                 }
-                if ($personne->getConjoint() !== null) {
-                    $conjoint = $personne->getConjoint();
-                    $personne->setConjoint(null);
+
+                $conjoint = $personne->getConjoint();
+                $primary  = $this->em->getRepository(Personne::class)->findOneBy(['conjoint' => $personne->getId()]);
+
+                if($conjoint instanceof Personne) {
                     $conjoint->setConjoint(null);
-                    $this->em->persist($personne);
                     $this->em->persist($conjoint);
-                    $this->em->flush();
                 }
+
+                if($primary instanceof Personne) {
+                    $primary->setConjoint(null);
+                    $this->em->persist($primary);
+                }
+
+                $personne->setConjoint(null);
+                $this->em->persist($personne);
+                $this->em->flush();
+
+                // Récupération du ticket
+                if($personne->getTicket() !== null) {
+                    $tickets[] = $personne->getTicket();
+                }
+
                 $this->em->remove($personne);
             }
+            $this->em->flush();
+
+            // Suppression des tickets
             if (count($tickets) > 0) {
                 foreach ($tickets as $ticket) {
                     $fichier = $ticket->getFichier();
@@ -176,13 +193,12 @@ class ResetController extends AbstractController
                 }
             }
             $this->em->flush();
-            $this->em->commit();
             $this->addFlash('success', "Suppression terminée !");
         } catch (\Exception $exception) {
-            $this->em->rollback();
             $this->addFlash('error', "Une erreur est survenue lors de la suppression des données.");
         }
 
-        return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        // return on current page
+        return $this->redirectToRoute('home');
     }
 }
