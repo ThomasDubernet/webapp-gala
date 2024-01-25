ac6b0d03b16a24abae37afccf54293a2acc317b5
ThomasDubernet <thoms.dubernet@gmail.com>
2024-01-25 11:07:43 +0100

    [+] - Migration - Ajout du ticketId de billet web

---
+++ migrations/Version20240125091439.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240125091439 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE personne ADD billet_web_ticket_id VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE personne DROP billet_web_ticket_id');
+    }
+}
